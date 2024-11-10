6919a121ed948ef730f22a76d4aec465c3acafda
SvetaKozyreva <svtkozyreva@gmail.com>
2024-11-10 16:34:49 +0200

    1

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -7,10 +7,15 @@ namespace Lab06
         {
             Random r = new Random();
 
+            double geom = 1;
+            double average = 0;
             int[] massive = new int[20];
+
+            Console.Write($"Масив: ");
             for (int i = 0; i < massive.Length; i++)
             {
                 massive[i] = r.Next(1,10);
+                Console.Write(massive[i] + " ");
             }
 
             Thread T0 = new Thread(Average);
@@ -19,27 +24,28 @@ namespace Lab06
             T0.Start();
             T1.Start();
 
-            
+            T0.Join();
+            T1.Join();
+
+            Console.WriteLine($"\nСереднє арифметичне: {average}");
+            Console.WriteLine($"Середнє геометричне: {geom}");
 
             void Average()
             {
-                double average = 0;
-
                 for (int i = 0; i < massive.Length; i++)
                 {
                     average += massive[i];
                 }
-                average = average / 2;
+                average /= 2;
             }
+
             void Geom()
             {
-                double geom = 1;
-
                 for (int i = 0; i < massive.Length; i++)
                 {
                     geom *= massive[i];
                 }
-                geom = Math.Pow(geom, 2);
+                geom = Math.Round(Math.Sqrt(geom), 1);
             }
 
 
